346b45a4489650d252d22e5018b63772cd5d5528
Samuel Mindler <mindlersamuel@gmail.com>
2023-09-16 19:19:48 -0300

    listo?

--- build/generated-sources/ap-source-output/logica/Usuario_.java
+++ build/generated-sources/ap-source-output/logica/Usuario_.java
@@ -5,7 +5,7 @@ import javax.annotation.processing.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-09-16T17:52:34", comments="EclipseLink-2.7.12.v20230209-rNA")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2023-09-16T19:19:32", comments="EclipseLink-2.7.12.v20230209-rNA")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
